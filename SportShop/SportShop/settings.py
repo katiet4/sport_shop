6ab701b69913ddc101b81b0191ff3fd705217bfb
katiet4 <anton_16_78_01_01@mail.ru>
2019-11-30 01:09:04 +0300

    .

--- SportShop/SportShop/settings.py
+++ SportShop/SportShop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nnck3!gv$z1i!hpa)_e%7fk_x6s=8l_+w9v=r&(*j42xlkv$g@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.5","127.0.0.1"]
 
 
 # Application definition
